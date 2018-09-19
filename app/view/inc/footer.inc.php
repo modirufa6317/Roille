415e2e94f9e3f6fbf1ae847e8da9d8a2a38c0eea
modirufa6317 <34741799+modirufa6317@users.noreply.github.com>
2018-09-19 10:20:20 +0200

    Add files via upload

---
+++ app/view/inc/footer.inc.php
@@ -0,0 +1,2 @@
+</div> <!-- End of the Main Container-->
+<script type="text/javascript" src="../../web/js/checkPass.js"><script>
---
+++ app/view/inc/menu.inc.php
@@ -0,0 +1,47 @@
+<!-- Main Container-->
+<div class="container-fluid">
+    <!-- Main navigation bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <a class="navbar-brand" href="#">Navbar</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Connexion</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inscription</a>
+                </li>
+
+                <li class="nav-item dropdown">
+
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Machines</a>
+                        <a class="dropdown-item" href="#">Outillage</a>
+                        <a class="dropdown-item" href="#">Matériels</a>
+                    </div>
+
+                </li>
+
+            </ul>
+
+        </div>
+        
+    </nav> <!-- End of the Main Navigation Bar -->
+
